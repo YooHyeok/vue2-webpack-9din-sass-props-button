062380ca9821fcecec578d06c582d311b8063eb2
webdevyoo <yjk7454@naver.com>
2025-06-15 18:40:50 +0900

    feat: Content 컴포넌트 퍼블리싱 - Button 컴포넌트 적용 및 props 세팅

---
+++ src/components/organisms/Content.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="content">
+    <div class="content__text-box">
+      <span class="content__text-box__title">How to start :</span>
+      <div class="content__text-box__subTitle-box">
+        <span class="subTitle">1. Create a page</span>
+        <span class="subTitle">2. Add boards to page</span>
+      </div>
+      <Button
+        :theme="'secondary'"
+        label="Add New Page"
+        class="content__text-box__button" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from '~/components/atoms/Button.vue'
+
+export default {
+    components: { Button },
+    setup() {
+        return {}
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/styles/main.scss';
+
+.content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 855px;
+    height: 100vh; // 1080px
+
+    border-right: 1px solid #e6e6e6;
+    background-color: #f9f9f9;
+
+    &__text-box {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        width: 182px;
+
+        gap: 19px;
+
+        &__title {
+            font-size: 28px;
+            font-weight: 700;
+        }
+        &__subTitle-box {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+
+            .subTitle {
+                font-size: 18px;
+                font-weight: 500;
+                line-height: 32px;
+            }
+        }
+        &__button {
+            width: 100%;
+        }
+    }
+}
+</style>
